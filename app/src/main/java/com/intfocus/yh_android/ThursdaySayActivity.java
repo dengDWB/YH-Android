f8b27a0b23a0770f3cb1a425c9487b830ea12367
Liurl21 <liurl21@outlook.com>
2016-10-12 12:24:29 +0800

    fixed: 小四说闪退BUG

--- app/src/main/java/com/intfocus/yh_android/ThursdaySayActivity.java
+++ app/src/main/java/com/intfocus/yh_android/ThursdaySayActivity.java
@@ -1,10 +1,18 @@
 package com.intfocus.yh_android;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.RelativeLayout;
 
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.handmark.pulltorefresh.library.PullToRefreshWebView;
 import com.intfocus.yh_android.util.K;
+import com.intfocus.yh_android.util.LogUtil;
 import com.intfocus.yh_android.util.PrivateURLs;
 import com.intfocus.yh_android.util.URLs;
 
@@ -16,14 +24,52 @@ public class ThursdaySayActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thursday_say);
-        pullToRefreshWebView = (PullToRefreshWebView) findViewById(R.id.browser);
-        initWebView();
-        setPullToRefreshWebView(true);
 
-        mWebView.requestFocus();
-        mWebView.loadUrl(urlStringForLoading);
+        initThursdaySayWebView();
+
+        animLoading.setVisibility(View.VISIBLE);
         urlString = String.format(K.kThursdaySayMobilePath, PrivateURLs.kBaseUrl, URLs.currentUIVersion(ThursdaySayActivity.this));
         new Thread(mRunnableForDetecting).start();
+    }
+
+    private void initThursdaySayWebView() {
+        mWebView = (WebView) findViewById(R.id.browser);
+        mWebView.requestFocus();
+        animLoading = (RelativeLayout) findViewById(R.id.anim_loading);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDefaultTextEncodingName("utf-8");
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+
+        mWebView.setWebChromeClient(new WebChromeClient());
+        mWebView.setDrawingCacheEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(android.webkit.WebView view, String url) {
+                //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                LogUtil.d("onPageStarted", String.format("%s - %s", URLs.timestamp(), url));
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                animLoading.setVisibility(View.GONE);
+                LogUtil.d("onPageFinished", String.format("%s - %s", URLs.timestamp(), url));
+            }
+
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                LogUtil.d("onReceivedError",
+                        String.format("errorCode: %d, description: %s, url: %s", errorCode, description,
+                                failingUrl));
+            }
+        });
     }
 
     public void dismissActivity(View v) {
