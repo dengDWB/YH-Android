cf158b2e6694522bc02f291b5f77375292079380
dengwenbin <402847229@qq.com>
2016-10-14 15:57:23 +0800

    fixed:消息推送

---
+++ app/src/main/java/com/intfocus/yonghuitest/ThursdaySayActivity.java
@@ -0,0 +1,76 @@
+package com.intfocus.yonghuitest;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.RelativeLayout;
+
+import com.intfocus.yonghuitest.util.K;
+import com.intfocus.yonghuitest.util.LogUtil;
+import com.intfocus.yonghuitest.util.PrivateURLs;
+import com.intfocus.yonghuitest.util.URLs;
+
+/**
+ * Created by 40284 on 2016/9/10.
+ */
+public class ThursdaySayActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thursday_say);
+
+        initThursdaySayWebView();
+
+        animLoading.setVisibility(View.VISIBLE);
+        urlString = String.format(K.kThursdaySayMobilePath, PrivateURLs.kBaseUrl, URLs.currentUIVersion(ThursdaySayActivity.this));
+        new Thread(mRunnableForDetecting).start();
+    }
+
+    private void initThursdaySayWebView() {
+        mWebView = (WebView) findViewById(R.id.browser);
+        mWebView.requestFocus();
+        animLoading = (RelativeLayout) findViewById(R.id.anim_loading);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDefaultTextEncodingName("utf-8");
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+
+        mWebView.setWebChromeClient(new WebChromeClient());
+        mWebView.setDrawingCacheEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(android.webkit.WebView view, String url) {
+                //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                LogUtil.d("onPageStarted", String.format("%s - %s", URLs.timestamp(), url));
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                animLoading.setVisibility(View.GONE);
+                LogUtil.d("onPageFinished", String.format("%s - %s", URLs.timestamp(), url));
+            }
+
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                LogUtil.d("onReceivedError",
+                        String.format("errorCode: %d, description: %s, url: %s", errorCode, description,
+                                failingUrl));
+            }
+        });
+    }
+
+    public void dismissActivity(View v) {
+        ThursdaySayActivity.this.onBackPressed();
+    }
+}
