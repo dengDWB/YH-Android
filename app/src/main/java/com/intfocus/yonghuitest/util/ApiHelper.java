23140d0b072c6dd1107a9fe3df6885f171081b41
jay16 <jay_li@intfocus.com>
2016-11-03 20:39:55 +0800

    refactor(push#token): reset push_valid=NO when upgrade app

--- app/src/main/java/com/intfocus/yonghuitest/util/ApiHelper.java
+++ app/src/main/java/com/intfocus/yonghuitest/util/ApiHelper.java
@@ -431,20 +431,20 @@ public class ApiHelper {
             String pushConfigPath = String.format("%s/%s", FileUtil.basePath(context), K.kPushConfigFileName);
             JSONObject pushJSON = FileUtil.readConfigFile(pushConfigPath);
 
-            if(pushJSON.has(K.kPushValid) && pushJSON.getBoolean(K.kPushValid) && pushJSON.has(URLs.kPushDeviceToken) && pushJSON
-                .getString(URLs.kPushDeviceToken).length() == 44) return true;
-            if(pushJSON.has(URLs.kPushDeviceToken) && pushJSON.getString(URLs.kPushDeviceToken).length() != 44) return false;
+            if(pushJSON.has(K.kPushIsValid) && pushJSON.getBoolean(K.kPushIsValid) && pushJSON.has(K.kPushDeviceToken) && pushJSON
+                .getString(K.kPushDeviceToken).length() == 44) return true;
+            if(pushJSON.has(K.kPushDeviceToken) && pushJSON.getString(K.kPushDeviceToken).length() != 44) return false;
 
-            if(pushJSON.has(URLs.kPushDeviceToken)) {
-                String urlString = String.format(K.kPushDeviceTokenAPIPath, K.kBaseUrl, deviceUUID, pushJSON.getString(URLs.kPushDeviceToken));
+            if(pushJSON.has(K.kPushDeviceToken)) {
+                String urlString = String.format(K.kPushDeviceTokenAPIPath, K.kBaseUrl, deviceUUID, pushJSON.getString(K.kPushDeviceToken));
                 Map<String, String> response = HttpUtil.httpPost(urlString, new JSONObject());
                 JSONObject responseJSON = new JSONObject(response.get(URLs.kBody));
 
-                pushJSON.put(K.kPushValid,
-                    responseJSON.has(K.kValid) && responseJSON.getBoolean(K.kValid));
+                pushJSON.put(K.kPushIsValid, responseJSON.has(K.kValid) && responseJSON.getBoolean(K.kValid));
+                pushJSON.put(K.kPushDeviceUUID, deviceUUID);
                 FileUtil.writeFile(pushConfigPath, pushJSON.toString());
 
-                return pushJSON.has(K.kPushValid) && pushJSON.getBoolean(K.kPushValid);
+                return pushJSON.has(K.kPushIsValid) && pushJSON.getBoolean(K.kPushIsValid);
             } else {
                 return false;
             }
